9f52be75ff7871b153dd20a9755f388cfe592ee1
Kalifunk <nathansing.ns@gmail.com>
2023-01-14 15:53:44 +1100

    added edge case

--- 11-golang-wordsearch/wordsearch.go
+++ 11-golang-wordsearch/wordsearch.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
@@ -21,29 +22,39 @@ func Solve(words []string, puzzle []string) (map[string][2][2]int, error) {
 	
 	for i := 0; i<len(puzzle); i++ {
         for j :=0;j<len(puzzle[0]); j++ {
-
-						dynamString := [3][3]string{}
-						for k:=0;k<maxLen;k++ {
-								for _,off := range offset {
-									nr,nc := off[0]*k, off[1]*k
-									dsr, dsc := off[0], off[1]
-									if dsr == -1 {
-										dsr = 2
-									}
-									if dsc == -1 {
-										dsc = 2
-									}
-									if (i+nr >= 0 && i+nr < len(puzzle)) && (j+nc >= 0 && j+nc < len(puzzle[0])) {
-										dynamString[dsr][dsc] += string(puzzle[i+nr][j+nc])
-										_, ok := wordset[dynamString[dsr][dsc]]
-										if ok {
-											output[dynamString[dsr][dsc]] = [2][2]int{{j,i},{j+nc, i+nr}}
-										}
-									}
-								}		
-						}
+            dynamString := [3][3]string{}
+            for k:=0;k<maxLen;k++ {
+                for _,off := range offset {
+                    nr,nc := off[0]*k, off[1]*k
+                    dsr, dsc := off[0], off[1]
+                    if dsr == -1 {
+                        dsr = 2
+                    }
+                    if dsc == -1 {
+                        dsc = 2
+                    }
+                    if (i+nr >= 0 && i+nr < len(puzzle)) && (j+nc >= 0 && j+nc < len(puzzle[0])) {
+                        dynamString[dsr][dsc] += string(puzzle[i+nr][j+nc])
+                        _, ok := wordset[dynamString[dsr][dsc]]
+                        if ok {
+                            output[dynamString[dsr][dsc]] = [2][2]int{{j,i},{j+nc, i+nr}}
+                        }
+                    }
+                }		
+            }
         }
-		}
+	}
+	shouldError := false
+	for searchword,_ := range wordset {
+        _, ok := output[searchword]
+        if (!ok) {
+            output[searchword] = [2][2]int{[2]int{-1,-1}, [2]int{-1,-1}}
+            shouldError = true
+        }
+    }
+	if (shouldError) {
+        return output,errors.New("this is an error object.")
+    }
 	return output,nil
 }
 func main() {
