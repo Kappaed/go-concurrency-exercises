781b4c2785bcaa01420209223a8b8f95813dba80
Kalifunk <nathansing.ns@gmail.com>
2023-01-14 15:34:04 +1100

    added word search

---
+++ 11-golang-wordsearch/wordsearch.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+
+
+func Solve(words []string, puzzle []string) (map[string][2][2]int, error) {
+	wordset := make(map[string]bool)
+	output := make(map[string][2][2]int)
+	offset := [][2]int{{1,0},{-1,0},{0,-1},{0,1},{1,1},{-1,1},{1,-1},{-1,-1}}
+	maxLen := math.MinInt32
+	for _,word := range words {
+        wordset[word] = true
+        if len(word) > maxLen {
+            maxLen = len(word)
+        }
+    }
+	
+	for i := 0; i<len(puzzle); i++ {
+        for j :=0;j<len(puzzle[0]); j++ {
+
+						dynamString := [3][3]string{}
+						for k:=0;k<maxLen;k++ {
+								for _,off := range offset {
+									nr,nc := off[0]*k, off[1]*k
+									dsr, dsc := off[0], off[1]
+									if dsr == -1 {
+										dsr = 2
+									}
+									if dsc == -1 {
+										dsc = 2
+									}
+									if (i+nr >= 0 && i+nr < len(puzzle)) && (j+nc >= 0 && j+nc < len(puzzle[0])) {
+										dynamString[dsr][dsc] += string(puzzle[i+nr][j+nc])
+										_, ok := wordset[dynamString[dsr][dsc]]
+										if ok {
+											output[dynamString[dsr][dsc]] = [2][2]int{{j,i},{j+nc, i+nr}}
+										}
+									}
+								}		
+						}
+        }
+		}
+	return output,nil
+}
+func main() {
+	puzzle :=  []string{"jefblpepre", "camdcimgtc", "oivokprjsm", "pbwasqroua", "rixilelhrs", "wolcqlirpc", "screeaumgr", "alxhpburyi", "jalaycalmp", "clojurermt"}
+	words :=  []string{"clojure", "elixir", "ecmascript", "rust", "java", "lua"}
+		coords, _ := Solve(words, puzzle)
+		for key,value := range coords {
+			scoord, ecoord := value[0], value[1]
+			fmt.Println(key,":",scoord,",",ecoord)
+		}
+}
